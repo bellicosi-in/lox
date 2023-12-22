e6f2a63016635607610f16635194acad57474396
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2023-12-22 10:32:26 +0530

    created the initial stage of dealing with chunks of bytecode

---
+++ chunk.h
@@ -0,0 +1,13 @@
+#include "common.h"
+
+
+typedef enum{
+    OP_RETURN,
+}OpCode;
+
+
+typedef struct{
+    int count;
+    int capacity;
+    uint8_t* code;
+}Chunk;
---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef clox_common_h
+
+#define clox_common_h
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif
